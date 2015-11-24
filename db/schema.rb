b26dc13193d2ce93d0ab20bf39b587f5c68c4549
Danny Rosenfeld <dannyarosenfeld@gmail.com>
2015-11-23 17:23:04 -0800

    Review scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123211940) do
+ActiveRecord::Schema.define(version: 20151124010637) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20151123211940) do
   end
 
   add_index "links", ["user_id"], name: "index_links_on_user_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
